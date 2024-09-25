6305ba2a959789d5b5919bc23ce79ecf6bb82167
weinzbw <130248057+weinzbw@users.noreply.github.com>
2024-09-25 10:36:04 -0500

    Create ai.py from Del

---
+++ ai.py
@@ -0,0 +1,19 @@
+class AI():
+    def __init__(self):
+        # ask user to select a difficulty
+        # 1 = easy 2 = med 3 = hard
+        # then set self.difficulty depending on what is returned
+        pass
+    
+    def easy_shoot(self):
+        pass # easy behavior goes here (returns coords)
+    
+    def med_shoot(self):
+        pass # med behavior (returns coords)
+    
+    def hard_shoot(self):
+        pass # hard behavior (returns coords)
+    
+    def turn(self):
+        # depending on self.difficulty, call one of the three methods above
+        pass
